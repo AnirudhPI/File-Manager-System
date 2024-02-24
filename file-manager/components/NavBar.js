379af59c466dc01b59b991c0774a07f646dc24ad
AnirudhPI <anirudhinamdar@gmail.com>
2024-02-24 13:38:49 -0700

    Added NavBar component

---
+++ file-manager/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './NavBar.module.css'; 
+import Image from 'next/image';
+import IDM from '../assets/Internet-Download-Manager.webp';
+
+const NavBar = () => {
+  return (
+    <nav className={styles.navBar}>
+        <div className={styles.navRow}>
+            <div className={styles.nameHolder}>
+            <Image src={IDM} alt="Internet Download Manager Logo"
+                width={25} // Set the width as needed
+                height={25} // Set the height as needed
+                objectFit="contain" 
+                />
+                <div className={styles.title}>
+                    Internet Download Manager 10.36
+                </div>
+            </div>
+        <div className={styles.menu}>
+            <a className={styles.menuItem} href="#">Tasks</a>
+            <a className={styles.menuItem} href="#">File</a>
+            <a className={styles.menuItem} href="#">Downloads</a>
+            <a className={styles.menuItem} href="#">View</a>
+            <a className={styles.menuItem} href="#">Help</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
